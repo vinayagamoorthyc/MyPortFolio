9ab672d06b515c971d266b8e04c7a774a85c83b7
vinayagamoorthyc <sanjaiiti2709@gmail.com>
2024-04-11 13:38:48 +0530

    0.0.0.2

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,7 @@ import "./Home.css"
 import About from './components/About'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
-import Bottom from './components/Bottom'
+import Bottom from './components/Bottom';
 
 export default function Home() {
   return (
